71be33cebd33bfd6c781e947e119e08b69694b09
Marek Piotrowski-EXT <Marek.Piotrowski@jeppesen.com>
2020-02-28 17:16:54 +0100

    add unit test

--- github/githubTests/GithubMainViewModelSpec.swift
+++ github/githubTests/GithubMainViewModelSpec.swift
@@ -7,19 +7,27 @@
 //
 
 import XCTest
+import RxTest
+import RxSwift
+import RxBlocking
 @testable import github
 
 class GithubMainViewModelSpec: XCTestCase {
+    
+    var sut : GithubMainViewModel!
 
-    override func setUp() {
+    func testProvidingDataFromGithub() {
+        let searchResult = SearchResult(totalCount: 5, incompleteResults: false, items: [])
+        let mockGithubSearchProvider = MockGithubSearchProvider()
+        mockGithubSearchProvider.stubbedResult = Observable<SearchResult>.just(searchResult)
+        sut = GithubMainViewModel(provider: mockGithubSearchProvider)
+        TestHelper.doInTheSecond {
+            self.sut.search("githubsearch")
+        }
+        let result = try? sut.searchResult().toBlocking(timeout: 3).first()
+        XCTAssertEqual(result?.totalCount, 5, "Should be the same value")
+        XCTAssertTrue(result?.items?.isEmpty ?? false)
     }
-
-    override func tearDown() {
-
-    }
-
-    func testExample() {
-    }
-
-
 }
+
+
